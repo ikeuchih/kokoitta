69f747569d4f6bad2543a1d3dd3ff1fd512c8885
ikeuchih <ikeuchi.hinano@gmail.com>
2021-04-11 07:38:28 +0000

    risize

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -4,9 +4,20 @@ class Micropost < ApplicationRecord
   mount_uploader :picture, PictureUploader
   validates :user_id, presence: true
   validates :content, presence: true
+  validate  :picture_size
+
   
 
  # マイクロポストは1人のユーザーと紐づく
  # ユーザーID・コンテンツが空白の場合はマイクロポストできないように
+ 
+ private
+
+    # アップロードされた画像のサイズをバリデーションする
+    def picture_size
+      if picture.size > 5.megabytes
+        errors.add(:picture, "should be less than 5MB")
+      end
+    end
 
 end
